9fdfceb1018bcac4535790f4f385140f9397fd57
unknown <pantho883@gmail.com>
2026-07-11 04:00:48 +0600

    dropdown added

--- src/components/layout/header/Dropdown.jsx
+++ src/components/layout/header/Dropdown.jsx
@@ -4,14 +4,27 @@ import {
   NativeSelectOptGroup,
   NativeSelectOption,
 } from "@/components/ui/native-select"
+
+const items =[
+    {label:'computer',value:'computer'},
+    {label:'laptop',value:'laptop'},
+    {label:'smartphone',value:'smartphone'},
+    {label:'gaming console',value:'gaming console'},
+    {label:'monitors',value:'monitors'},
+]
 const Dropdown = () => {
   return (
-<div className='text-primary-black'><NativeSelect>
-  <NativeSelectOption value="">All Category</NativeSelectOption>
+<div className='text-primary-black'>
+<NativeSelect>
+  {/* <NativeSelectOption value="">All Category</NativeSelectOption>
   <NativeSelectOption value="apple">Apple</NativeSelectOption>
   <NativeSelectOption value="banana">Banana</NativeSelectOption>
   <NativeSelectOption value="blueberry">Blueberry</NativeSelectOption>
-  <NativeSelectOption value="pineapple">Pineapple</NativeSelectOption>
+  <NativeSelectOption value="pineapple">Pineapple</NativeSelectOption> */}
+  <NativeSelectOption value="">All Category</NativeSelectOption>
+  {items.map((item)=>(
+  
+  <NativeSelectOption key={item.value} value={item.value}>{item.label}</NativeSelectOption>))}
 </NativeSelect>
 </div>
   )
